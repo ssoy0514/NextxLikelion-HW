0437e59ae38716d553ae245f30d93f14056bb5e8
ssoy0514 <badr1108@naver.com>
2022-05-09 16:15:18 +0900

    HW

---
+++ Session8_HW/subjectProject/subjectApp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Major(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.name)
+
+class Subject(models.Model):
+    major = models.ForeignKey("Major", related_name="subject", on_delete=models.CASCADE)
+    subject_name = models.CharField(max_length=200)
+    prof_name = models.CharField(max_length=200)
+    memo = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.subject_name)
